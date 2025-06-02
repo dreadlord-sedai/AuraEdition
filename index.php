3b5609ed830253b370e30b0f1eab75d16de2b494
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-02 09:53:38 +0530

    Featured vehicles section

--- index.php
+++ index.php
@@ -105,27 +105,73 @@
         <h2 class="text-start mb-4">Featured Vehicles</h2>
         <div class="row">
 
-            <div class="col-3 mb-4">
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
                 <div class="card">
-                    <img src="./public/images/featured-1.jpg" class="card-img-top" alt="Featured Vehicle">
+                    <img src="./products/img/feature1.jpg" class="card-img-top" alt="Featured Vehicle">
                     <div class="card-body">
                         <h5 class="card-title">2023 Lamborghini Huracan</h5>
                         <p class="card-text">$250,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
                     </div>
                 </div>
             </div>
-            <div class="col-3 mb-4">
-                <div class="card">
-                    <img src="./public/images/featured-2.jpg" class="card-img-top" alt="Featured Vehicle">
-                    <div class="card-body">
-                        <h5 class="card-title">2022 Ferrari 488 GTB</h5>
-                        <p class="card-text">$280,000</p>                    
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <img src="./products/img/feature1.jpg" class="card-img-top" alt="Featured Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                        <p class="card-text">$250,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <img src="./products/img/feature1.jpg" class="card-img-top" alt="Featured Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                        <p class="card-text">$250,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <img src="./products/img/feature1.jpg" class="card-img-top" alt="Featured Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                        <p class="card-text">$250,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <img src="./products/img/feature1.jpg" class="card-img-top" alt="Featured Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                        <p class="card-text">$250,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <img src="./products/img/feature1.jpg" class="card-img-top" alt="Featured Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                        <p class="card-text">$250,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+            
+    </div>
     <!-- Featured Vehicles Section -->
 
 
