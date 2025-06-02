d895b383a1607f38cffe733f4c1b9dc02a1e12b3
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-02 09:06:00 +0530

    Styles order

--- includes/header.php
+++ includes/header.php
@@ -7,10 +7,12 @@
     <title>Home</title>
 
     <!-- Stylesheets -->
-    <link rel="stylesheet" href="./public/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="shortcut icon" href="./public/images/favicon.ico" type="image/x-icon">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    
+    <link rel="stylesheet" href="./public/css/style.css">
 </head>
 
     
