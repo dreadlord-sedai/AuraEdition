20d584526f691c0d05b194b9170abdba00d1b0c3
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-02 08:52:50 +0530

    hero Section

--- index.php
+++ index.php
@@ -1,12 +1,47 @@
 <?php include_once 'includes/header.php'; ?>
 
 <body>
-<?php include_once 'includes/navbar.php'; ?>
+
+    <!-- Navigation Bar -- -->
+    <?php include_once 'includes/navbar.php'; ?>
+    <!-- Navigation Bar -->
+
+    <!-- Hero Section -->
+    <div class="">
+        <img src="./public/images/hero-img.png" alt="Hero" class="img-fluid w-100">
+        <div class="position-absolute top-50 alighn-items-start text-white ms-5 ">
+            <h1>AuraEdition</h1>
+            <p>Explore 31,000+ luxury cars, supercars and exotic cars for sale worldwide in one
+                simple search</p>
+        </div>
+    </div>
+
+    <!-- Hero Section -->
+
+    <!-- Popular Makes Section -->
+    <div class="container-md my-5">
+        <h2 class="text-start mb-4">Popular Makes</h2>
+        <div class="row">
+            <div class="col-2 mb-4">
+                <div class="card aspect-ratio ratio-1x1 bg-black">
+                        <img src="./public/images/hero-img.png" alt="">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Popular Makes Section -->
 
 
+    <!-- Featured Vehicles Section -->
 
-<!-- Scripts -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js
+    <!-- Featured Vehicles Section -->
+
+
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js
 "></script>
 </body>
 
