577215c6c3aae01332531c2ff7517e036c10d5df
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-02 11:10:17 +0530

    Add absolute links and restructure Project

--- includes/footer.php
+++ includes/footer.php
@@ -4,8 +4,10 @@
 
 
 <!-- Scripts -->
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js
 "></script>
+<script src="/Projects/AuraEdition/assets/js/script.js"></script>
 </body>
 
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,12 @@
 
 
     <!-- Stylesheets -->
-    <link rel="shortcut icon" href="./public/images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/Projects/AuraEdition/assets/images/favicon.ico" type="image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     
-    <link rel="stylesheet" href="./public/css/style.css">
+    <link rel="stylesheet" href="/Projects/AuraEdition/assets/css/style.css">
 </head>
 
     
