ebdb04724c016c9b7c4f6dbd07ad2513a81c1943
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-02 08:21:21 +0530

    Create Seperate navbar and header files and link to index

--- includes/navbar.php
+++ includes/navbar.php
@@ -8,7 +8,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="#">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="listings.php">Vehicles</a>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php include_once 'includes/header.php'; ?>
+
+<body>
 <?php include_once 'includes/navbar.php'; ?>
 
 
