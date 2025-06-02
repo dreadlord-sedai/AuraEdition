11a81d96626af269d07777d0d1de63148200ede2
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-02 08:12:10 +0530

    Create Seperate navbar and header files and link to index

---
+++ includes/navbar.php
@@ -0,0 +1,39 @@
+<!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div class="container-md">
+            <a class="navbar-brand" href="#">AuraEdition</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="listings.php">Vehicles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="categories.php">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                </ul>
+
+                <div class="ms-auto d-flex align-items-center">
+                    <a href="cart.php">
+                        <i class="bi bi-cart-fill fs-3 me-3 text-light"></i>
+                    </a>
+                    <button class="btn btn-outline-success d-flex align-items-center" type="submit">
+                        Login
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    </nav>
+    <!-- Navigation Bar -->
