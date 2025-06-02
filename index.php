087c6196d8ec93696a6145cceb5ed8b4f44b0ff7
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-02 07:58:31 +0530

    Create Navbar

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- Stylesheets -->
     <link rel="stylesheet" href="./public/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="./public/images/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
