5db8497af68d8d358985ed860679b7ac7bb4c889
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-02 10:10:53 +0530

    Footer section

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer class="bg-dark text-white text-center py-3 w-100">
+    <p>&copy; 2025 AuraEdition. All rights reserved.</p>
+</footer>
+
+
+<!-- Scripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js
+"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -170,89 +170,83 @@
                     </div>
                 </div>
             </div>
-            
+
+        </div>
     </div>
-    <!-- Featured Vehicles Section -->
+        <!-- Featured Vehicles Section -->
 
-    <!-- Popular Vehicles Section -->
-    <div class="container-md my-5">
-        <h2 class="text-start mb-4">Popular</h2>
-        <div class="row">
+        <!-- Popular Vehicles Section -->
+        <div class="container-md my-5">
+            <h2 class="text-start mb-4">Popular</h2>
+            <div class="row">
 
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card">
-                    <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
-                    <div class="card-body">
-                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
-                        <p class="card-text">$250,000</p>
-                        <a href="#" class="btn btn-primary w-100">View</a>
+                <div class="col-12 col-sm-6 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
+                        <div class="card-body">
+                            <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                            <p class="card-text">$250,000</p>
+                            <a href="#" class="btn btn-primary w-100">View</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
+                        <div class="card-body">
+                            <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                            <p class="card-text">$250,000</p>
+                            <a href="#" class="btn btn-primary w-100">View</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
+                        <div class="card-body">
+                            <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                            <p class="card-text">$250,000</p>
+                            <a href="#" class="btn btn-primary w-100">View</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
+                        <div class="card-body">
+                            <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                            <p class="card-text">$250,000</p>
+                            <a href="#" class="btn btn-primary w-100">View</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
+                        <div class="card-body">
+                            <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                            <p class="card-text">$250,000</p>
+                            <a href="#" class="btn btn-primary w-100">View</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
+                        <div class="card-body">
+                            <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                            <p class="card-text">$250,000</p>
+                            <a href="#" class="btn btn-primary w-100">View</a>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+            <!-- Popular Vehicles Section -->
 
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card">
-                    <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
-                    <div class="card-body">
-                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
-                        <p class="card-text">$250,000</p>
-                        <a href="#" class="btn btn-primary w-100">View</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card">
-                    <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
-                    <div class="card-body">
-                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
-                        <p class="card-text">$250,000</p>
-                        <a href="#" class="btn btn-primary w-100">View</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card">
-                    <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
-                    <div class="card-body">
-                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
-                        <p class="card-text">$250,000</p>
-                        <a href="#" class="btn btn-primary w-100">View</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card">
-                    <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
-                    <div class="card-body">
-                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
-                        <p class="card-text">$250,000</p>
-                        <a href="#" class="btn btn-primary w-100">View</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card">
-                    <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
-                    <div class="card-body">
-                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
-                        <p class="card-text">$250,000</p>
-                        <a href="#" class="btn btn-primary w-100">View</a>
-                    </div>
-                </div>
-            </div>
-            
-    </div>
-    <!-- Popular Vehicles Section -->
-
-
-    
-    <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js
-"></script>
-</body>
-
-</html>
+            <?php include_once 'includes/footer.php'; ?>
