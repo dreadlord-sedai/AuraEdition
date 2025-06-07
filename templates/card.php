7014c0842b3b6c3c25fcece579c145c95d05b052
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-07 11:53:06 +0530

    Add Wishlist Button

--- templates/card.php
+++ templates/card.php
@@ -2,20 +2,20 @@
 
 <div class="container-md my-5">
     <h2 class="text-start mb-4">Featured </h2>
-    <div class="row"></div>
-
-    <!-- Product Card -->
-    <div class="col-12 col-sm-6 col-md-4 mb-4">
-        <div class="card">
-            <img src="/Projects/AuraEdition/products/img/feature1.jpg" class="card-img-top" alt="Featured Vehicle">
-            <div class="card-body">
-                <h5 class="card-title">2023 Lamborghini Huracan</h5>
-                <p class="card-text">$250,000</p>
-                <a href="#" class="btn btn-primary w-100">View</a>
+    <div class="row">
+        <!-- Product Card -->
+        <div class="col-12 col-sm-6 col-md-4 mb-4">
+            <div class="card">
+                <img src="/Projects/AuraEdition/products/img/feature1.jpg" class="card-img-top" alt="Featured Vehicle">
+                <div class="card-body">
+                    <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                    <p class="card-text">$250,000</p>
+                    <a href="#" class="btn btn-primary w-100">View</a>
+                </div>
             </div>
         </div>
+        <!-- Product Card -->
     </div>
-    <!-- Product Card -->
 
 </div>
 
