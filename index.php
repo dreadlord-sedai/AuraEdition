6a91db8a806d7fb2bcd758f25228f951183ee00d
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-07 10:38:00 +0530

    Create card template

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AuraEdition | Home</title>
     
-<?php include_once 'includes/header.php'; ?>
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/header.php'; ?>
 
 <body>
 
     <!-- Navigation Bar -- -->
-    <?php include_once 'includes/navbar.php'; ?>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/navbar.php'; ?>
     <!-- Navigation Bar -->
 
     <!-- Hero Section -->
