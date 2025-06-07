d7a8f1bc44cd5c61e5dde066cdfed4a3c1e69e92
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-07 11:55:51 +0530

    Complete Listings page Structure

--- products/listings.php
+++ products/listings.php
@@ -36,7 +36,112 @@
                     </div>
                 </div>
             </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <button class="wishlist-button btn btn-outline-light position-absolute top-0 end-0 m-2 p-2 rounded-circle shadow-sm">
+                        <i class="bi bi-heart mt-1"></i>
+                    </button>
+                    <img src="/Projects/AuraEdition/products/img/listings1.jpg" class="card-img-top" alt="Listings Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2024 Mustang GT 500</h5>
+                        <p class="card-text">$150,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <button class="wishlist-button btn btn-outline-light position-absolute top-0 end-0 m-2 p-2 rounded-circle shadow-sm">
+                        <i class="bi bi-heart mt-1"></i>
+                    </button>
+                    <img src="/Projects/AuraEdition/products/img/listings1.jpg" class="card-img-top" alt="Listings Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2024 Mustang GT 500</h5>
+                        <p class="card-text">$150,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
             
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <button class="wishlist-button btn btn-outline-light position-absolute top-0 end-0 m-2 p-2 rounded-circle shadow-sm">
+                        <i class="bi bi-heart mt-1"></i>
+                    </button>
+                    <img src="/Projects/AuraEdition/products/img/listings1.jpg" class="card-img-top" alt="Listings Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2024 Mustang GT 500</h5>
+                        <p class="card-text">$150,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <button class="wishlist-button btn btn-outline-light position-absolute top-0 end-0 m-2 p-2 rounded-circle shadow-sm">
+                        <i class="bi bi-heart mt-1"></i>
+                    </button>
+                    <img src="/Projects/AuraEdition/products/img/listings1.jpg" class="card-img-top" alt="Listings Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2024 Mustang GT 500</h5>
+                        <p class="card-text">$150,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <button class="wishlist-button btn btn-outline-light position-absolute top-0 end-0 m-2 p-2 rounded-circle shadow-sm">
+                        <i class="bi bi-heart mt-1"></i>
+                    </button>
+                    <img src="/Projects/AuraEdition/products/img/listings1.jpg" class="card-img-top" alt="Listings Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2024 Mustang GT 500</h5>
+                        <p class="card-text">$150,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <button class="wishlist-button btn btn-outline-light position-absolute top-0 end-0 m-2 p-2 rounded-circle shadow-sm">
+                        <i class="bi bi-heart mt-1"></i>
+                    </button>
+                    <img src="/Projects/AuraEdition/products/img/listings1.jpg" class="card-img-top" alt="Listings Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2024 Mustang GT 500</h5>
+                        <p class="card-text">$150,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <button class="wishlist-button btn btn-outline-light position-absolute top-0 end-0 m-2 p-2 rounded-circle shadow-sm">
+                        <i class="bi bi-heart mt-1"></i>
+                    </button>
+                    <img src="/Projects/AuraEdition/products/img/listings1.jpg" class="card-img-top" alt="Listings Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2024 Mustang GT 500</h5>
+                        <p class="card-text">$150,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-4">
+                <div class="card">
+                    <button class="wishlist-button btn btn-outline-light position-absolute top-0 end-0 m-2 p-2 rounded-circle shadow-sm">
+                        <i class="bi bi-heart mt-1"></i>
+                    </button>
+                    <img src="/Projects/AuraEdition/products/img/listings1.jpg" class="card-img-top" alt="Listings Vehicle">
+                    <div class="card-body">
+                        <h5 class="card-title">2024 Mustang GT 500</h5>
+                        <p class="card-text">$150,000</p>
+                        <a href="#" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
             
 
 
