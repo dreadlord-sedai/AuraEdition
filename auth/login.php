75bd6732cbb98fa946b4ce6084eeb73f65c40dfe
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-08 14:01:24 +0530

    Code Login page

--- auth/login.php
+++ auth/login.php
@@ -14,22 +14,22 @@
         <img src="/Projects/AuraEdition/assets/images/sign.jpg" class="w-full h-screen object-cover" alt="Sign Image">
         <div class="absolute inset-0 flex justify-center items-center">
             <div class="w-full max-w-md">
-                <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <h2 class="text-2xl font-bold mb-4">Login</h2>
+                <div class="bg-black/40 backdrop-blur shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <h2 class="text-2xl font-bold mb-4 text-white">Login</h2>
                     <form action="/Projects/AuraEdition/auth/login.php" method="POST">
                         <div class="mb-4">
-                            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                            <label for="email" class="block text-white text-sm font-bold mb-2">Email</label>
                             <input type="email" id="email" name="email" required
-                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline">
                         </div>
                         <div class="mb-2">
-                            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                            <label for="password" class="block text-white text-sm font-bold mb-2">Password</label>
                             <input type="password" id="password" name="password" required
-                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline">
                         </div>
                         <div class="mb-6 text-right">
                             <a href="/Projects/AuraEdition/auth/forgot_password.php"
-                               class="text-blue-500 hover:text-blue-700 text-sm">Forgot your password?</a>
+                               class="text-blue-300 hover:text-blue-400 text-sm">Forgot your password?</a>
                         </div>
                         <div class="flex flex-col gap-3">
                             <button type="submit"
