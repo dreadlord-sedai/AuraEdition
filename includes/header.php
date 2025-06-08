ce4ad863860e190285dccb1ff82015faf41b5924
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-08 10:49:02 +0530

    Add Tailwind

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,7 @@
 <!-- Stylesheets -->
     <link rel="shortcut icon" href="/Projects/AuraEdition/assets/images/favicon.ico" type="image/x-icon">
 
+    <link rel="stylesheet" href="/Projects/AuraEdition/assets/css/tailwind-output.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
