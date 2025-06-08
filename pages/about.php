9eca483d6ddfbaba80b59dd614e812adab7f4ee6
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-08 10:36:35 +0530

    Create about page

---
+++ pages/about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AuraEdition | About</title>
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/header.php'; ?>
+
+<body>
+
+    <!-- Navigation Bar -- -->
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/navbar.php'; ?>
+    <!-- Navigation Bar -->
+
+    <!-- Hero Section -->
+    <div class="Hero">
+        
+        </div>
+    </div>
+    <!-- Hero Section -->
+
+
+    <div class="container-md my-5 main-content">
+
+        <!-- About Section -->
+
+    </div>
+
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . "/Projects/AuraEdition/includes/footer.php"; ?>
