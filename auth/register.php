013c313675fdaeb6db75b90918c6b7dc38d8c829
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-08 14:06:20 +0530

    Code Register Page

---
+++ auth/register.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AuraEdition | Register</title>
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/header.php'; ?>
+
+<body>
+
+    <div class="relative w-full h-screen">
+        <img src="/Projects/AuraEdition/assets/images/sign.jpg" class="w-full h-screen object-cover" alt="Sign Image">
+        <div class="absolute inset-0 flex justify-center items-center">
+            <div class="w-full max-w-md">
+                <div class="bg-black/40 backdrop-blur shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <h2 class="text-2xl font-bold mb-4 text-white">Login</h2>
+                    <form action="/Projects/AuraEdition/auth/login.php" method="POST">
+                        <div class="grid grid-cols-2 gap-4 mb-4">
+                            <div class="">
+                                <label for="fname" class="block text-white text-sm font-bold mb-2">First Name</label>
+                                <input type="fname" id="fname" name="fname" required
+                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline">
+                            </div>
+                            <div class="">
+                                <label for="lname" class="block text-white text-sm font-bold mb-2">Last Name</label>
+                                <input type="lname" id="lname" name="lname" required
+                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline">
+                            </div>
+                        </div>
+                        <div class="mb-4">
+                            <label for="email" class="block text-white text-sm font-bold mb-2">Email</label>
+                            <input type="email" id="email" name="email" required
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline">
+                        </div>
+                        <div class="mb-2">
+                            <label for="password" class="block text-white text-sm font-bold mb-2">Password</label>
+                            <input type="password" id="password" name="password" required
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline">
+                        </div>
+                        <div class="flex flex-col gap-3">
+                            <button type="submit"
+                                class="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                Register
+                            </button>
+                            <a href="/Projects/AuraEdition/auth/register.php"
+                                class="w-full">
+                                <button type="button"
+                                    class="w-full bg-gray-100 hover:bg-blue-100 text-blue-700 font-bold py-2 px-4 rounded border border-blue-500 focus:outline-none focus:shadow-outline">
+                                    Login
+                                </button>
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js
+"></script>
+    <script src="/Projects/AuraEdition/assets/js/script.js"></script>
+
+</body>
+
+</html>
