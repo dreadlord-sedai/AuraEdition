f761617842aa9cf576102a9735b51466752ade3e
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-08 13:40:59 +0530

    Bug fix: footer alignment

--- index.php
+++ index.php
@@ -134,34 +134,37 @@
 
 
 
-            
-    <!-- Featured Vehicles Section -->
 
-    <!-- Popular Vehicles Section -->
-    <div class="container-md my-5">
-        <h2 class="text-start mb-4">Popular</h2>
-        <div class="row">
+            <!-- Featured Vehicles Section -->
 
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card">
-                    <button class="wishlist-button btn btn-outline-light position-absolute top-0 end-0 m-2 p-2 rounded-circle shadow-sm">
-                        <i class="bi bi-heart mt-1"></i>
-                    </button>
-                    <a href="/Projects/AuraEdition/products/productDetails.php">
-                        <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">2023 Lamborghini Huracan</h5>
-                        <p class="card-text">$250,000</p>
-                        <div class="d-flex gap-2">
-                            <a href="/Projects/AuraEdition/products/productDetails.php" class="btn btn-primary">Buy Now</a>
-                            <a href="/Projects/AuraEdition/pages/cart.php" class="btn btn-primary"> Add to Cart</a>
+            <!-- Popular Vehicles Section -->
+            <div class="container-md my-5">
+                <h2 class="text-start mb-4">Popular</h2>
+                <div class="row">
+
+                    <div class="col-12 col-sm-6 col-md-4 mb-4">
+                        <div class="card">
+                            <button class="wishlist-button btn btn-outline-light position-absolute top-0 end-0 m-2 p-2 rounded-circle shadow-sm">
+                                <i class="bi bi-heart mt-1"></i>
+                            </button>
+                            <a href="/Projects/AuraEdition/products/productDetails.php">
+                                <img src="./products/img/feature2.jpg" class="card-img-top" alt="Featured Vehicle">
+                            </a>
+                            <div class="card-body">
+                                <h5 class="card-title">2023 Lamborghini Huracan</h5>
+                                <p class="card-text">$250,000</p>
+                                <div class="d-flex gap-2">
+                                    <a href="/Projects/AuraEdition/products/productDetails.php" class="btn btn-primary">Buy Now</a>
+                                    <a href="/Projects/AuraEdition/pages/cart.php" class="btn btn-primary"> Add to Cart</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 
-            
     <!-- Popular Vehicles Section -->
 
     <?php include_once $_SERVER['DOCUMENT_ROOT'] . "/Projects/AuraEdition/includes/footer.php"; ?>
