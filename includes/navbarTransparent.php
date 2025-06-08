0c2731d461cc5736f54b412ba790752f6910b21e
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-08 11:19:24 +0530

    Create transparent navbar

---
+++ includes/navbarTransparent.php
@@ -0,0 +1,43 @@
+<!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg bg-transparent" data-bs-theme="dark">
+        <div class="container-md ">
+            <a class="navbar-brand logo" href="/Projects/AuraEdition/index.php">AuraEdition</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/Projects/AuraEdition/index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/Projects/AuraEdition/products/listings.php">Listings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/Projects/AuraEdition/pages/categories.php">Makes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/Projects/AuraEdition/pages/about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/Projects/AuraEdition/pages/contact.php">Contact</a>
+                    </li>
+                </ul>
+
+                <div class="ms-auto d-flex align-items-center">
+                    <a href="/Projects/AuraEdition/pages/cart.php">
+                        <i class="bi bi-cart-fill fs-3 me-3 text-light"></i>
+                    </a>
+
+                    <a href="/Projects/AuraEdition/auth/login.php" class="text-decoration-none text-light">
+                        <button class="btn btn-outline-success d-flex align-items-center">
+                        Login
+                    </button>
+                    </a>
+                    
+                </div>
+            </div>
+
+        </div>
+    </nav>
+    <!-- Navigation Bar -->
