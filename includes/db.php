a2abfb6e0976f7df0f7abe25611e2eaf4bbc307a
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-09 10:55:24 +0530

    Connenct databse

---
+++ includes/db.php
@@ -0,0 +1,20 @@
+<?php 
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = 'mysql2006';
+$db_name = 'auraedition';
+
+// Enable mysqli error reporting for development
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try {
+    $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    echo "Database connection successful.";
+    // Connection successful
+} catch (mysqli_sql_exception $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
+
+?>
