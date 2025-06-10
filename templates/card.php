52291ca16ff4bbe15159acced2f2584a0ab5612c
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-10 12:24:20 +0530

    Code get All makes card template

--- templates/card.php
+++ templates/card.php
@@ -68,26 +68,38 @@
     <div class="container-md my-5 main-content">
         <h2 class="text-start mb-4">Makes</h2>
         <div class="row">
+
+        <?php
+        include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/db.php';
+        include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/functions.php';
+        $makes = getAllMakes($connection);
+        ?>
+
             <!-- Makes Card -->
+            <?php foreach ($makes as $make) : ?>
             <div class="col-12 col-sm-6 col-md-4 mb-4">
                 <div class="card makes-card  d-flex flex-row align-items-center gap-3">
-                    <img src="/Projects/AuraEdition/products/img/makes1.jpg" class="makesCard-img" alt="Featured Vehicle">
+                    <img src="<?=$make['make_image']; ?>" class="makesCard-img" alt="Featured Vehicle">
                     <div class="d-flex flex-column justify-content-center flex-grow-1">
 
                         <div class="pb-4">
-                            <p class="makesCard-title mb-1">Dodge</p>
+                            <p class="makesCard-title mb-1"><?=$make['make_name']; ?></p>
                         </div>
 
                         <div class="d-flex justify-content-between align-items-center">
                             <p class="card-text mb-0 text-muted" style="font-size: 0.95rem;">120 Listings</p>
-                            <button class="makesCard-btn btn btn-outline-light d-flex justify-content-center align-items-center">
+                            <a href="/Projects/AuraEdition/pages/categories.php?id=<?= $make['make_id'] ?>" class="text-decoration-none">
+                                <button class="makesCard-btn btn btn-outline-light d-flex justify-content-center align-items-center">
                                 <i class="fa-solid fa-arrow-right"></i>
                             </button>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
+            <?php endforeach; ?>
             <!-- Makes Card -->
+             
         </div>
     </div>
 
