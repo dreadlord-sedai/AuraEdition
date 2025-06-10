f962f1bf8b3e099d01df2e30851aec946d765502
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-10 08:26:54 +0530

    create Product details page

---
+++ products/productDetails.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AuraEdition | Contact</title>
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/header.php'; ?>
+
+<body>
+
+    <!-- Navigation Bar -- -->
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/navbar.php'; ?>
+    <!-- Navigation Bar -->
+
+    <div class="container-md my-5 main-content">
+
+        <!-- Product Details Section -->
+         <div class="flex flex-row gap-6 justify-content-center">
+            <div class="flex-1 bg-white/10 rounded-lg shadow-lg p-6">
+                
+                <img src="/Projects/AuraEdition/products/img/product.jpg" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="">
+            </div>
+            <div class="flex-1 bg-white/10 rounded-lg shadow-lg p-6">
+                <!-- Box 2 content -->
+                <h3 class="text-xl font-bold mb-2 text-white">Box 2</h3>
+                <p class="text-white/80">Content for the second box.</p>
+            </div>
+        </div>
+        <!-- Product Details Section -->
+
+        
+
+    </div>
+
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . "/Projects/AuraEdition/includes/footer.php"; ?>
