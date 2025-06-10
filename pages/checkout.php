e5e392dd25f63873e33e44f1dc8c1c70dddcfe2e
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-10 20:27:53 +0530

    Checkout card

--- pages/checkout.php
+++ pages/checkout.php
@@ -21,24 +21,124 @@
         <!-- Checkout -->
         <div class="flex flex-row gap-3 justify-content-between rounded-lg align-items-start bg-gray-500 p-3">
 
-            <div class="flex flex-col w-3/5 justify-content-center rounded-lg align-items-start bg-gray-200 p-5">
+            <div class="flex flex-col w-3/5 gap-2 justify-content-center rounded-lg align-items-start bg-gray-200 p-5">
                 <h4>Checkout</h4>
 
-                <div class="flex flex-row gap-3 justify-content-center rounded-md align-items-start bg-gray-400 p-3">
-                    <div class="flex flex-row w-1/2 justify-content-center  rounded-sm ">
-                        <img src="/Projects/AuraEdition/assets/images/checkout1.jpg" class="img-fluid object-fit-cover" alt="">
+                <!-- Item Card -->
+                <div class="flex flex-row gap-3 justify-content-center rounded-md align-items-start bg-gray-400 p-4">
+
+                    <div class="justify-content-center rounded-sm ">
+                        <img src="/Projects/AuraEdition/assets/images/checkout1.jpg" class="img-fluid object-fit-cover aspect-square w-24 " alt="">
                     </div>
+
+                    <div class="flex flex-row gap-4">
+                        <div class="w-3/4 mt-2 ">
+                            <h5>2023 Lamborghini Huracan</h5>
+                        </div>
+
+                        <div class="flex flex-col w-1/4 justify-content-center align-items-center">
+                            <div class="mb-2 mt-3">
+                                <h5>$100</h5>
+                            </div>
+
+                            <div class="flex flex-row gap-2">
+                                <button class="btn btn-primary">-</button>
+                                <h5>1</h5>
+                                <button class="btn btn-primary">+</button>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
+                <div class="flex flex-row gap-3 justify-content-center rounded-md align-items-start bg-gray-400 p-4">
+
+                    <div class="justify-content-center rounded-sm ">
+                        <img src="/Projects/AuraEdition/assets/images/checkout1.jpg" class="img-fluid object-fit-cover aspect-square w-24 " alt="">
+                    </div>
+
+                    <div class="flex flex-row gap-4">
+                        <div class="w-3/4 mt-2 ">
+                            <h5>2023 Lamborghini Huracan</h5>
+                        </div>
+
+                        <div class="flex flex-col w-1/4 justify-content-center align-items-center">
+                            <div class="mb-2 mt-3">
+                                <h5>$100</h5>
+                            </div>
+
+                            <div class="flex flex-row gap-2">
+                                <button class="btn btn-primary">-</button>
+                                <h5>1</h5>
+                                <button class="btn btn-primary">+</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="flex flex-row gap-3 justify-content-center rounded-md align-items-start bg-gray-400 p-4">
+
+                    <div class="justify-content-center rounded-sm ">
+                        <img src="/Projects/AuraEdition/assets/images/checkout1.jpg" class="img-fluid object-fit-cover aspect-square w-24 " alt="">
+                    </div>
+
+                    <div class="flex flex-row gap-4">
+                        <div class="w-3/4 mt-2 ">
+                            <h5>2023 Lamborghini Huracan</h5>
+                        </div>
+
+                        <div class="flex flex-col w-1/4 justify-content-center align-items-center">
+                            <div class="mb-2 mt-3">
+                                <h5>$100</h5>
+                            </div>
+
+                            <div class="flex flex-row gap-2">
+                                <button class="btn btn-primary">-</button>
+                                <h5>1</h5>
+                                <button class="btn btn-primary">+</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="flex flex-row gap-3 justify-content-center rounded-md align-items-start bg-gray-400 p-4">
+
+                    <div class="justify-content-center rounded-sm ">
+                        <img src="/Projects/AuraEdition/assets/images/checkout1.jpg" class="img-fluid object-fit-cover aspect-square w-24 " alt="">
+                    </div>
+
+                    <div class="flex flex-row gap-4">
+                        <div class="w-3/4 mt-2 ">
+                            <h5>2023 Lamborghini Huracan</h5>
+                        </div>
+
+                        <div class="flex flex-col w-1/4 justify-content-center align-items-center">
+                            <div class="mb-2 mt-3">
+                                <h5>$100</h5>
+                            </div>
+
+                            <div class="flex flex-row gap-2">
+                                <button class="btn btn-primary">-</button>
+                                <h5>1</h5>
+                                <button class="btn btn-primary">+</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- Item Card -->
+
             </div>
-        
 
 
-        <div class="flex flex-row w-2/5 justify-content-between rounded-lg align-items-center bg-gray-200 p-5">
+
+            <div class="flex flex-row w-2/5 justify-content-between rounded-lg align-items-center bg-gray-200 p-5">
+
+            </div>
 
         </div>
-
-    </div>
 
 
 
