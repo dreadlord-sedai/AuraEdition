99079a361ba991ca5f0554e9490ccd60139aee81
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-10 11:26:23 +0530

    Code get_vehicle_image function

--- includes/functions.php
+++ includes/functions.php
@@ -15,3 +15,21 @@ function get_featured_vehicles($connection, $limit = 3)
     $select_Featured->close();
     return $featured_vehicles;
 }
+
+
+function get_vehicle_image($vehicle_id, $connection)
+{
+    // Fetch vehicle image
+    $select_image = $connection->prepare(
+        "SELECT image FROM vehicle_images WHERE vehicle_id = ? LIMIT 1"
+    );
+    $select_image->bind_param("i", $vehicle_id);
+    $select_image->execute();
+    $result = $select_image->get_result();
+    
+    if ($result && $row = $result->fetch_assoc()) {
+        return $row['image'];
+    }
+    
+    return null; // Return null if no image found
+}
