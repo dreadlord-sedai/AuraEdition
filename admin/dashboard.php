86ea8b0ce3d8de5ae3f842a7986fd6650325d24f
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-11 12:21:19 +0530

    Admin Dashboard

---
+++ admin/dashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AuraEdition | Dashboard</title>
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/header.php'; ?>
+
+<body>
+
+    <!-- Navigation Bar -- -->
+    <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/admin/includes/adminNavbar.php'; ?>
+    <!-- Navigation Bar -->
+
+    <!--SideBar
+
+    
+
+
+    <div class="container-md my-5 main-content">
+
+    
+
+    </div>
+
+
