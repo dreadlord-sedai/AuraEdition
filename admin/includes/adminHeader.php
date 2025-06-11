11676ef37f519a48f02d9bee9ee5d93877d96a07
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-11 12:40:04 +0530

    Admin Header

--- admin/includes/adminHeader.php
+++ admin/includes/adminHeader.php
@@ -2,10 +2,8 @@
     <link rel="shortcut icon" href="/Projects/AuraEdition/assets/images/favicon.ico" type="image/x-icon">
 
     
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="/Projects/AuraEdition/assets/css/tailwind-output.css">
-    <link rel="stylesheet" href="/Projects/AuraEdition/assets/css/style.css">
-    
+    <link rel="stylesheet" href="/Projects/AuraEdition/admin/assets/css/adminStyles.css">
 </head>
 
