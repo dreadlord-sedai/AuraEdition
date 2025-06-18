60fd9d4de5bb8161e0dbf5611c51581704e31d7d
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-18 13:41:58 +0530

    Cart Page

--- pages/cart.php
+++ pages/cart.php
@@ -18,10 +18,10 @@
     <!-- Main Content -->
     <div class="container-md my-5 main-content">
 
-        <div class="flex flex-col justify-content-center align-items-center">
+        <div class="flex flex-col justify-content-center align-items-center gap-10">
             <!-- Cart Card -->
             <div class="flex flex-col w-75 flex-grow gap-3 justify-content-center rounded-lg align-items-center bg-gray-200 p-5">
-                <h4>Checkout</h4>
+                <h4>Cart</h4>
 
                 <!-- Item Card -->
                 <div class="flex flex-row justify-between gap-3 items-center rounded-md bg-gray-400 p-4 w-full">
@@ -50,10 +50,104 @@
 
                     </div>
                 </div>
+
+                <div class="flex flex-row justify-between gap-3 items-center rounded-md bg-gray-400 p-4 w-full">
+                    <div class="rounded-sm">
+                        <img src="/Projects/AuraEdition/assets/images/checkout1.jpg" class="img-fluid object-fit-cover aspect-square w-40" alt="">
+                    </div>
+
+                    <div class="flex flex-col items-center">
+                        <div class="">
+                            <h5>2023 Lamborghini Huracan</h5>
+                        </div>
+                        <div class="mb-2">
+                            <h5>$100</h5>
+                        </div>
+
+                        <div class="flex flex-row gap-2">
+                            <button class="btn btn-primary">-</button>
+                            <h5 class="mx-2">1</h5>
+                            <button class="btn btn-primary">+</button>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col items-center gap-2">
+                        <button class="btn btn-outline-success">Buy Now</button>
+                        <button class="btn btn-outline-danger">Remove</button>
+
+                    </div>
+                </div>
+
+                <div class="flex flex-row justify-between gap-3 items-center rounded-md bg-gray-400 p-4 w-full">
+                    <div class="rounded-sm">
+                        <img src="/Projects/AuraEdition/assets/images/checkout1.jpg" class="img-fluid object-fit-cover aspect-square w-40" alt="">
+                    </div>
+
+                    <div class="flex flex-col items-center">
+                        <div class="">
+                            <h5>2023 Lamborghini Huracan</h5>
+                        </div>
+                        <div class="mb-2">
+                            <h5>$100</h5>
+                        </div>
+
+                        <div class="flex flex-row gap-2">
+                            <button class="btn btn-primary">-</button>
+                            <h5 class="mx-2">1</h5>
+                            <button class="btn btn-primary">+</button>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col items-center gap-2">
+                        <button class="btn btn-outline-success">Buy Now</button>
+                        <button class="btn btn-outline-danger">Remove</button>
+
+                    </div>
+                </div>
+
+                <div class="flex flex-row justify-between gap-3 items-center rounded-md bg-gray-400 p-4 w-full">
+                    <div class="rounded-sm">
+                        <img src="/Projects/AuraEdition/assets/images/checkout1.jpg" class="img-fluid object-fit-cover aspect-square w-40" alt="">
+                    </div>
+
+                    <div class="flex flex-col items-center">
+                        <div class="">
+                            <h5>2023 Lamborghini Huracan</h5>
+                        </div>
+                        <div class="mb-2">
+                            <h5>$100</h5>
+                        </div>
+
+                        <div class="flex flex-row gap-2">
+                            <button class="btn btn-primary">-</button>
+                            <h5 class="mx-2">1</h5>
+                            <button class="btn btn-primary">+</button>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col items-center gap-2">
+                        <button class="btn btn-outline-success">Buy Now</button>
+                        <button class="btn btn-outline-danger">Remove</button>
+
+                    </div>
+                </div>
+
+                <div class="flex flex-row justify-between gap-3 items-center rounded-md bg-gray-400 p-4 w-full px-5">
+                    <h4>Total:</h4>
+                    <h4>$400</h4>
+                </div>
                 <!-- Item Card -->
 
             </div>
             <!-- Cart Card -->
+
+            <!-- Buttons -->
+            <div class="flex flex-row gap-3 w-full justify-center p-5 my-4 border-y-1 border-gray-400 ">
+                <a href="/Projects/AuraEdition/pages/checkout.php" class="btn btn-primary">PURCHASED HISTORY</a>
+                <a href="/Projects/AuraEdition/pages/index.php" class="btn btn-primary">WATCHLIST</a>
+            </div>
+            <!-- Buttons -->
+
         </div>
 
 
