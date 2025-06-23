5be547de044a3ef7198c191f4e682a9ee998f13c
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-23 10:24:14 +0530

    Remove From Wishlist

--- assets/js/script.js
+++ assets/js/script.js
@@ -332,3 +332,25 @@ function setupCartPageQuantityButtons() {
     });
 }
 /* CART */
+
+
+function removeFromWishlist(id) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            if (request.status == 200) {
+                var response = request.responseText.trim();
+                if (response === "success") {
+                    location.reload();
+                } else {
+                    alert("Remove from cart failed: " + response);
+                }
+            } else {
+                alert("Request failed with status " + request.status);
+            }
+        }
+    }
+    request.open("POST", "/Projects/AuraEdition/process/removeFromWishlistProcess.php", true);
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.send("id=" + encodeURIComponent(id));
+}
