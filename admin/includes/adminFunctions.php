f902dd78b740e0e87bc53f386ca0d716c826e5cb
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-23 14:20:14 +0530

    Account page

--- admin/includes/adminFunctions.php
+++ admin/includes/adminFunctions.php
@@ -1,8 +1,20 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/functions.php';
 
-function getUserInfo($connection, $user_id) {
-   $stmt = $connection->prepare("SELECT * FROM users WHERE id = ?");
+function getUserInfo($connection, $user_id)
+{
+    $stmt = $connection->prepare("SELECT
+    u.id,
+    u.fname,
+    u.lname,
+    u.email,
+    u.role,
+    ua.address,
+    ua.city,
+    ua.state
+FROM users u
+LEFT JOIN user_addresses ua ON u.id = ua.address_user_id
+WHERE u.id = ?");
     if (!$stmt) return null;
     $stmt->bind_param("i", $user_id);
     $stmt->execute();
