5d1724fe02518ba5f2ae1e1f2e441b75a649b1af
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-23 10:28:55 +0530

    Remove From Wishlist

--- process/removeFromWishlistProcess.php
+++ process/removeFromWishlistProcess.php
@@ -1,7 +1,10 @@
 <?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/db.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/functions.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/session.php';
 
-if (isset($_POST['vehicle_id'])) {
-    $vehicle_id = $_POST['vehicle_id'];
+if (isset($_POST['id'])) {
+    $vehicle_id = $_POST['id'];
     removeFromWishlist($connection, $vehicle_id);
     echo "success";
 } else {
