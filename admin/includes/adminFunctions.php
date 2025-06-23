1a4622acc0792d51636c8ce31e895c715ce7a0ea
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-23 15:22:43 +0530

    Update Account

--- admin/includes/adminFunctions.php
+++ admin/includes/adminFunctions.php
@@ -29,7 +29,7 @@ WHERE u.id = ?");
 function updateAccount($connection, $user_id, $fname, $lname, $email, $confirm_password)
 {
     $hashedPassword = password_hash($confirm_password, PASSWORD_DEFAULT);
-    $stmt = $connection->prepare("UPDATE users SET fname = ?, lname = ?, email = ?, password = ? WHERE id = ?");
+    $stmt = $connection->prepare("UPDATE users SET fname = ?, lname = ?, email = ?, hashed_password = ? WHERE id = ?");
     if (!$stmt) return null;
     $stmt->bind_param("ssssi", $fname, $lname, $email, $hashedPassword, $user_id);
     $stmt->execute();
--- admin/process/handleAccount.php
+++ admin/process/handleAccount.php
@@ -1,14 +1,28 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/db.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/admin/includes/adminFunctions.php'; 
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/session.php';
 
+
+$user = isset($_SESSION['user_id']) ? getUserInfo($connection, $_SESSION['user_id']) : null;
+if (!$user) {
+    header("Location: /Projects/AuraEdition/index.php");
+    exit;
+}
+
+
+
+// Check if user is logged in
 if (isset($_POST['update_account'])) {
-    $fname = $_POST['name'];
-    $fname = $_POST['lname'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
     $confirm_password = $_POST['confirm_password'];
 
     // Update account information in the database
-    updateAccount($connection, $user['id'], $name, $email, $password, $confirm_password);
+    updateAccount($connection, $user['id'], $fname, $lname, $email, $confirm_password);
+
+    // Redirect back to the admin account page
+    header("Location: /Projects/AuraEdition/admin/pages/adminAccount.php");
+    exit;
 }
