998f4e9ee79f052b6a5a4648833ac797df32bdab
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-28 13:59:33 +0530

    Bug Fixes

--- process/updateCartQuantity.php
+++ process/updateCartQuantity.php
@@ -1,7 +1,5 @@
 <?php
-
-include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/db.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/session.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/bootstrap.php';
 
 header('Content-Type: application/json');
 
