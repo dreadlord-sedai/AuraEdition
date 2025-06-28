5cca2d7516dd0286a9ab94c550a9c406f69f64f4
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-28 12:01:43 +0530

    Bug Fixes

--- process/addToCartProcess.php
+++ process/addToCartProcess.php
@@ -1,5 +1,6 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/bootstrap.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/Projects/AuraEdition/includes/functions.php';
 
 if (isset($_POST['vehicle_id'])) {
     $vehicle_id = $_POST['vehicle_id'];
